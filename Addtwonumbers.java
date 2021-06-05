9b75379b5dafa7fb5e01bc3051c9832796f1d252
Mohamed Shameer A <66585290+shameerwahab786@users.noreply.github.com>
2021-06-05 17:53:16 +0530

    Create Addtwonumbers.java

---
+++ Addtwonumbers.java
@@ -0,0 +1,10 @@
+package number.programs;
+
+public class AddTwoNumbers {
+	public static void main(String[] args) {
+		int num1 = 40, num2 = 30, sum;
+		sum = num1 + num2;
+		System.out.println("Sum of two numbers : " +sum);
+		
+	}
+}

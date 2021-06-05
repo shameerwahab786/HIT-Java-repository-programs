23447c63912e5384187ffe69bbace4cd720b5dde
Mohamed Shameer A <66585290+shameerwahab786@users.noreply.github.com>
2021-06-05 19:53:21 +0530

    Update CheckEvenOdd.java

--- CheckEvenOdd.java
+++ CheckEvenOdd.java
@@ -1,11 +1,14 @@
 package number.programs;
 
-class CheckEvenOdd {   
-	public static void main(String args[])   {     
+class CheckEvenOdd
+{   
+	public static void main(String args[])  
+	{     
 		int a = 37;     
 		/* If number is divisible by 2 then it's an even number      * else odd number*/  
 		if ( a % 2 == 0 )         
 			System.out.println("Entered number is even");      
 		else         
 			System.out.println("Entered number is odd");   
-		} } 
+	} 
+} 
